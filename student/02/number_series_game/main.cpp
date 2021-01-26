dc10d677b931eda49edfbd1fbd28ed8c9919016c
Veikko Svanström <veikko.svanstrom@tuni.fi>
2021-01-26 12:02:33 +0200

    Syötteen lukeminen ja tulostus

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -4,4 +4,11 @@
 int main()
 {
     std::cout << "How many numbers would you like to have? ";
-}
+    int numbers;
+    std::cin >> numbers;
+
+    for ( int luku = 1; luku <= 10; ++luku) {
+        std::cout << luku << std::endl;
+        }
+    }
+
