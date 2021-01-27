554ec67c8dcb6f2579c4aa79c5c8a0a2fc678bb6
Veikko Svanström <veikko.svanstrom@tuni.fi>
2021-01-27 12:09:43 +0200

    Tekijä tehtävä

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
+
     cout << "Enter a positive number: ";
 
-    return 0;
+    int luku = 0;
+    std::cin >> luku;
+
+    if (luku <= 0) {
+        std::cout << "Only positive numbers accepted" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    int factor1 = 0;
+    int factor2 = 0;
+    int factor_cand = std::max(static_cast<int>(sqrt(luku)), 1);
+
+    for(; luku % factor_cand != 0; --factor_cand);
+
+        factor1 = factor_cand;
+        factor2 = luku / factor_cand;
+
+
+
+    std::cout << luku << " = " << factor1 << " * " << factor2 << std::endl;
+
+
+    return EXIT_SUCCESS;
 }
