aab4d918520bffe1cf1fca424337c48417e1444e
Veikko Svanström <veikko.svanstrom@tuni.fi>
2021-01-27 12:58:35 +0200

    korjaus2

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -10,12 +10,14 @@ int main()
     cin >> number;
 
     int cube = number * number * number;
-    if (cube / number / number != number) {
-        std::cout << "Error! The cube of " << number << " is not " << cube << endl;
+    if (number != 0){
+        if (cube / number / number != number) {
+            std::cout << "Error! The cube of " << number << " is not " << cube << endl;
+            return 0;
+        }
     }
-    else {
-        std::cout << "The cube of " << number << " is " << cube << "." << endl;
-    }
+
+    std::cout << "The cube of " << number << " is " << cube << "." << endl;
 
     return 0;
 }
