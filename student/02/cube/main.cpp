9ddc04c9927278b0a829e62b176760e17c12abe3
Veikko Svanström <veikko.svanstrom@tuni.fi>
2021-01-27 12:49:43 +0200

    Kuutio tehtävä

---
+++ student/02/cube/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    int cube = number * number * number;
+    if (cube / number / number != number) {
+        std::cout << "Error! The cube of " << number << " is not " << cube << endl;
+    }
+    else {
+        std:cout << "The cube of " << number << " is " << cube << "." << endl;
+    }
+
+    return 0;
+}
