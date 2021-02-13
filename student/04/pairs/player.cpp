eacd27901f7924be6b1a6f80d2156711c6a904b1
Veikko Svanström <veikko.svanstrom@tuni.fi>
2021-02-13 19:02:15 +0200

    card ja player luokat täydennetty, print funktio otettu käyttöön pääohjelmassa, alkukommentit tehty ja englannin kieliset kommentit poistettu

---
+++ student/04/pairs/player.cpp
@@ -0,0 +1,40 @@
+/* Ohjelman kirjoittaja
+ * Nimi: Veikko Svanstrom
+ * Opiskelijanumero: 050149539
+ * Kayttajatunnus: scvesv
+ * E-Mail: veikko.svanstrom@tuni.fi
+ */
+
+#include "player.hh"
+#include<iostream>
+
+// Lisää luokan toteutus tähän.
+// Kuhunkin julkiseen metodiin pitäisi riittää 1-2 koodiriviä.
+
+
+Player::Player(const std::string &name):
+name_(name), numb_of_pairs_(0)
+{
+
+}
+
+std::string Player::get_name() const
+{
+    return name_;
+}
+
+unsigned int Player::number_of_pairs() const
+{
+    return numb_of_pairs_;
+}
+
+void Player::add_card(Card &card)
+{
+    numb_of_pairs_ += 1;
+    card.remove_from_game_board();
+}
+
+void Player::print() const
+{
+    std::cout << "*** " << name_ << " has " << numb_of_pairs_ << " pair(s)." << std::endl;
+}
---
+++ student/04/pairs/player.hh
@@ -0,0 +1,54 @@
+/* Ohjelman kirjoittaja
+ * Nimi: Veikko Svanstrom
+ * Opiskelijanumero: 050149539
+ * Kayttajatunnus: scvesv
+ * E-Mail: veikko.svanstrom@tuni.fi
+ */
+
+/* Luokka: Player
+ * --------------
+ * Kuvaa yhtä pelaajaa muistipelissä.
+ *
+ * COMP.CS.110 K2021
+ * */
+//
+/* Class: Player
+ * -------------
+ * Represents a single player in pairs (memory) game.
+ *
+ * COMP.CS.110 K2021
+ * */
+
+#ifndef PLAYER_HH
+#define PLAYER_HH
+
+#include "card.hh"
+#include <string>
+
+class Player
+{
+public:
+    // Rakentaja: luo annetun nimisen pelaajan.
+    Player(const std::string& name);
+
+    // Palauttaa pelaajan nimen.
+    std::string get_name() const;
+
+    // Palauttaa pelaajan tähän asti keräämien parien määrän.
+    unsigned int number_of_pairs() const;
+
+    // Siirtää annetun kortin pelilaudalta pelaajalle,
+    // eli lisää kortin pelaajan keräämiin kortteihin
+    // ja poistaa sen pelilaudalta.
+    void add_card(Card& card);
+
+    // Tulostaa pelaajan tilanteen: nimen ja tähän asti kerättyjen parien määrän.
+    void print() const;
+
+private:
+    std::string name_;
+    unsigned int numb_of_pairs_;
+
+};
+
+#endif // PLAYER_HH
