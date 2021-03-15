fed85cc9916ae8798dc70550ecfe0194f28d1011
Veikko Svanström <veikko.svanstrom@tuni.fi>
2021-03-15 18:22:42 +0200

    Toteuttettu kaikki hakualgoritmit, joilla tietorakennetta voidaan käsitellä eli kaikki funktiot siis tehty. Myös split-funktiota muokattu toimimaan halutulla tavalla.

--- student/07/tramway2/main.cpp
+++ student/07/tramway2/main.cpp
@@ -1,3 +1,11 @@
+/*
+Rasse 2
+
+Kuvaus:
+Käynnistyessään ohjelma aluksi tulostaa
+
+*/
+
 #include <iostream>
 #include <map>
 #include <fstream>
@@ -21,23 +29,34 @@ void print_rasse()
                  "-------------------------------" << std::endl;
 }
 
-std::vector<std::string> split(const std::string& s, const char delimiter, bool ignore_empty = false){
-    std::vector<std::string> result;
-    std::string tmp = s;
+using Tramlines = std::map<std::string, std::map<double, std::string>>;
 
-    while(tmp.find(delimiter) != std::string::npos)
+std::vector<std::string> split(const std::string& s, const char delimiter){
+    std::vector<std::string> result = {""};
+    std::string tmp = s;
+    bool found = false;
+
+    for (char c : s)
     {
-        std::string new_part = tmp.substr(0, tmp.find(delimiter));
-        tmp = tmp.substr(tmp.find(delimiter)+1, tmp.size());
-        if(not (ignore_empty and new_part.empty()))
+        if (c == '"')
         {
-            result.push_back(new_part);
+            found = !found;
+        }
+        else if (c == delimiter and found == false)
+        {
+            result.push_back("");
+        }
+        else
+        {
+            result.back().push_back(c);
         }
     }
-    if(not (ignore_empty and tmp.empty()))
+
+    if (result.back() == "")
     {
-        result.push_back(tmp);
+        result.pop_back();
     }
+
     return result;
 }
 
@@ -46,7 +65,7 @@ std::string read_file_name()
 {
     std::string file_name;
     std::cout << "Give a name for input file: ";
-    std::cin >> file_name;
+    getline(std::cin, file_name);
     return file_name;
 }
 
@@ -77,65 +96,8 @@ bool check_data(std::string file_name)
     return true;
 }
 
-bool same_command(std::string real_command, std::string given_command)
+void save_data(Tramlines& tramlines, std::string file_name)
 {
-    std::transform(given_command.begin(), given_command.end(), given_command.begin(), ::toupper);
-    if (given_command == real_command)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-
-}
-
-using Tramlines = std::map<std::string, std::map<std::string, double>>;
-
-void print_lines(Tramlines tramlines)
-{
-    return;
-}
-
-void print_line_stops(Tramlines tramlines, std::string lines)
-{
-    return;
-}
-
-void print_stops(Tramlines tramlines)
-{
-    return;
-}
-
-void print_stop_lines(Tramlines tramlines, std::string stop)
-{
-    return;
-}
-
-void print_distance_between_stops(Tramlines tramlines, std::string line, std::string stop1, std::string stop2)
-{
-    return;
-}
-
-void add_line(Tramlines& tramlines, std::string line)
-{
-    return;
-}
-
-void add_stop(Tramlines& tramlines, std::string line, std::string new_stop, double distance)
-{
-    return;
-}
-
-void remove_stop(Tramlines& tramlines, std::string stop)
-{
-    return;
-}
-
-bool interface(std::string file_name)
-{
-    std::map <std::string, std::map<std::string, double>> tramlines;
     std::ifstream file(file_name);
     std::string line;
 
@@ -154,31 +116,297 @@ bool interface(std::string file_name)
         }
 
         double distance = stod(parts.at(2));
-        std::pair<std::string, double> stop_and_dis = {parts.at(1), distance};
+        std::pair<double, std::string> stop_and_dis = {distance, parts.at(1)};
 
         if (tramlines.find(parts.at(0)) == tramlines.end())
         {
             tramlines[parts.at(0)] = {};
         }
 
-        for (std::pair<std::string, double> stops : tramlines.at(parts.at(0)))
+        for (std::pair<double, std::string> stops : tramlines.at(parts.at(0)))
         {
-            if (stops.first == parts.at(1) or stops.second == distance)
+            if (stops.first == distance or stops.second == parts.at(1))
             {
                 std::cout << "Error: Stop/line already exists." << std::endl;
-                return false;
+                return;
             }
         }
         tramlines[parts.at(0)].insert(stop_and_dis);
     }
+    file.close();
+    return;
+}
+
+bool same_command(std::string real_command, std::string given_command)
+{
+    std::transform(given_command.begin(), given_command.end(), given_command.begin(), ::toupper);
+    if (given_command == real_command)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
 
 
+void print_lines(const Tramlines& tramlines)
+{
+    std::cout << "All tramlines in alphabetical order:" << std::endl;
+    std::vector<std::string> line_names = {};
+    for (std::pair<std::string, std::map<double, std::string>> lines : tramlines)
+    {
+        line_names.push_back(lines.first);
+    }
+    sort(line_names.begin(), line_names.end());
+    for (std::string s : line_names)
+    {
+        std::cout << s << std::endl;
+    }
+
+    return;
+}
+
+void print_line_stops(const Tramlines& tramlines, const std::string& line)
+{
+    bool line_found = false;
+
+    for (std::pair<std::string, std::map<double, std::string>> lines : tramlines)
+    {
+        if (lines.first == line)
+        {
+            line_found = true;
+        }
+    }
+
+    if (!line_found)
+    {
+        std::cout << "Error: Line could not be found." << std::endl;
+        return;
+    }
+
+    std::cout << "Line " << line << " goes through these stops in the order they are listed:" << std::endl;
+    for (std::pair<double, std::string> stops : tramlines.at(line))
+    {
+        std::cout << " - " << stops.second << " : " << stops.first << std::endl;
+    }
+    return;
+}
+
+void print_stops(const Tramlines& tramlines)
+{ 
+    std::cout << "All stops in alphabetical order:" << std::endl;
+    std::vector<std::string> stops = {};
+    for (std::pair<std::string, std::map<double, std::string>> lines : tramlines)
+    {
+        for (std::pair<double, std::string> stop : lines.second)
+        {
+            if (std::find(stops.begin(), stops.end(), stop.second) == stops.end())
+            {
+                stops.push_back(stop.second);
+            }
+        }
+
+    }
+    sort(stops.begin(), stops.end());
+    for (std::string s : stops)
+    {
+        std::cout << s << std::endl;
+    }
+    return;
+}
+
+void print_stop_lines(const Tramlines& tramlines, const std::string& stop)
+{
+    std::vector<std::string> line_names = {};;
+
+    for (std::pair<std::string, std::map<double, std::string>> lines : tramlines)
+    {
+        for (std::pair<double, std::string> s : lines.second)
+        {
+            if (s.second == stop)
+            {
+                line_names.push_back(lines.first);
+            }
+        }
+    }
+    sort(line_names.begin(), line_names.end());
+    if (line_names.size() == 0)
+    {
+        std::cout << "Error: Stop could not be found." << std::endl;
+    }
+    else
+    {
+        std::cout << "Stop " << stop << " can be found on the following lines:" << std::endl;
+        for (std::string l : line_names)
+        {
+            std::cout << " - " << l << std::endl;
+        }
+    }
+
+    return;
+}
+
+void print_distance_between_stops(const Tramlines& tramlines, const std::string& line, const std::string& stop1, const std::string& stop2)
+{
+    double dis1 = -1;
+    double dis2 = -1;
+    double dis_between = 0;
+
+    bool line_found = false;
+
+    for (std::pair<std::string, std::map<double, std::string>> lines : tramlines)
+    {
+        if (lines.first == line)
+        {
+            line_found = true;
+        }
+    }
+
+    if (!line_found)
+    {
+        std::cout << "Error: Line could not be found." << std::endl;
+        return;
+    }
+
+    for (std::pair<double, std::string> stops : tramlines.at(line))
+    {
+        if (stops.second == stop1)
+        {
+            dis1 = stops.first;
+        }
+        else if (stops.second == stop2)
+        {
+            dis2 = stops.first;
+        }
+    }
+
+    if (dis1 == -1 or dis2 == -1)
+    {
+        std::cout << "Error: Stop could not be found." << std::endl;
+        return;
+    }
+    else
+    {
+        if (dis1 > dis2)
+        {
+            dis_between = dis1 - dis2;
+        }
+        else
+        {
+            dis_between = dis2 - dis1;
+        }
+    }
+    std::cout << "Distance between " << stop1 << " and " << stop2 << " is " << dis_between << std::endl;
+
+    return;
+}
+
+void add_line(Tramlines& tramlines, const std::string& line)
+{
+    bool line_found = false;
+
+    for (std::pair<std::string, std::map<double, std::string>> lines : tramlines)
+    {
+        if (lines.first == line)
+        {
+            line_found = true;
+        }
+    }
+    if (line_found)
+    {
+        std::cout << "Error: Stop/line already exists." << std::endl;
+        return;
+    }
+
+    tramlines[line] = {};
+
+    return;
+}
+
+void add_stop(Tramlines& tramlines, const std::string& line, const std::string& new_stop, const double& distance)
+{
+    bool line_found = false;
+
+    for (std::pair<std::string, std::map<double, std::string>> lines : tramlines)
+    {
+        if (lines.first == line)
+        {
+            line_found = true;
+        }
+    }
+    if (!line_found)
+    {
+        std::cout << "Error: Line could not be found." << std::endl;
+        return;
+    }
+
+    for (std::pair<double, std::string> s : tramlines.at(line))
+    {
+        if (s.second == new_stop or s.first == distance)
+        {
+            std::cout << "Error: Stop/line already exists." << std::endl;
+            return;
+        }
+    }
+
+    std::pair<double, std::string> stop_and_dis = {distance, new_stop};
+    tramlines[line].insert(stop_and_dis);
+    std::cout << "Stop was added." << std::endl;
+    return;
+}
+
+void remove_stop(Tramlines& tramlines, const std::string& stop)
+{
+    bool stop_found = false;
+    for (std::pair<std::string, std::map<double, std::string>> lines : tramlines)
+    {
+        for (std::pair<double, std::string> s : lines.second)
+        {
+            if (s.second == stop)
+            {
+                stop_found = true;
+            }
+        }
+    }
+
+    if (!stop_found)
+    {
+        std::cout << "Error: Stop could not be found." << std::endl;
+        return;
+    }
+
+    for (std::pair<std::string, std::map<double, std::string>> lines : tramlines)
+    {
+        for (std::pair<double, std::string> s : lines.second)
+        {
+            if (s.second == stop)
+            {
+                tramlines.at(lines.first).erase(s.first);
+            }
+        }
+    }
+    std::cout << "Stop was removed from all lines." << std::endl;
+    return;
+}
+
+bool interface(std::string file_name)
+{
+    std::map <std::string, std::map<double, std::string>> tramlines;
+
+    save_data(tramlines, file_name);
     while(true)
     {
         std::string full_command;
         std::cout << "tramway> ";
         getline(std::cin, full_command);
         std::vector<std::string> command_parts = split(full_command, ' ');
+        if (command_parts.size() == 0)
+        {
+            continue;
+        }
         std::string command = command_parts.at(0);
 
         if (same_command("QUIT", command))
@@ -273,7 +501,7 @@ bool interface(std::string file_name)
         }
         else
         {
-            std::cout << "Error: Invalid input." << std::endl;
+            std::cout << "Error: Invalid input.1" << std::endl;
         }
     }
     return true;
