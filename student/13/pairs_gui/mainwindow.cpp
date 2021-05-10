9ad11042d1c3d7cc082285913c855079332c6a28
Veikko Svanström <veikko.svanstrom@tuni.fi>
2021-05-10 10:49:00 +0300

    Korttien toiminnallisuudet luoto. Pelaajien pisteenlasku myös toteutettu ja voittaj ilmoitetaan pelin lopussa.

--- student/13/pairs_gui/mainwindow.cpp
+++ student/13/pairs_gui/mainwindow.cpp
@@ -1,10 +1,7 @@
 #include "mainwindow.hh"
 #include "ui_mainwindow.h"
 #include <algorithm>
-#include <QGridLayout>
-#include <QPushButton>
-#include <QGraphicsView>
-#include <QLabel>
+
 
 
 
@@ -17,13 +14,17 @@ MainWindow::MainWindow(QWidget *parent)
     unsigned int rows = 1;
     unsigned int columns = 1;
 
+    ui->resetButton->setVisible(false);
     calculate_factors(rows, columns);
     init_cards(rows, columns);
     init_game_board(rows, columns);
+    init_graphics_views();
     connect(ui->CloseButton, &QPushButton::clicked,
             this, &MainWindow::handleCloseButtonClick);
     connect(ui->TurnBackButton, &QPushButton::clicked,
             this, &MainWindow::handleTurnBackButtonClick);
+    connect(ui->resetButton, &QPushButton::clicked,
+            this, &MainWindow::handeResetButtonClick);
 
 }
 
@@ -34,19 +35,19 @@ MainWindow::~MainWindow()
 
 void MainWindow::calculate_factors(unsigned int& smaller_factor, unsigned int& bigger_factor)
 {
-    for(unsigned int i = 1; i * i <= MAX_CARD_AMOUNT; ++i)
+    for(unsigned int i = 1; i * i <= CARD_AMOUNT; ++i)
     {
-        if(MAX_CARD_AMOUNT% i == 0)
+        if(CARD_AMOUNT% i == 0)
         {
             smaller_factor = i;
         }
     }
-    bigger_factor = MAX_CARD_AMOUNT / smaller_factor;
+    bigger_factor = CARD_AMOUNT / smaller_factor;
 }
 
 void MainWindow::init_cards(const unsigned int& rows, const unsigned int& columns)
 {
-    used_letters_ = letters.substr(0,MAX_CARD_AMOUNT);
+    used_letters_ = letters.substr(0,CARD_AMOUNT);
     random_shuffle(used_letters_.begin(), used_letters_.end());
 
     int counter = 0;
@@ -57,20 +58,22 @@ void MainWindow::init_cards(const unsigned int& rows, const unsigned int& column
         for (unsigned int j = 0; j < columns; ++j)
         {
             row.push_back(used_letters_.at(counter));
-            counter++;
+            ++counter;
         }
-        g_board_.push_back(row);
+        letters_g_board_.push_back(row);
     }
 }
 
 
 void MainWindow::handleCardClick()
 {
-    close();
-    /*if (turned_cards_ < 2)
+    if (turned_cards_ < 2)
     {
-
-    }*/
+        QPushButton* pressed_button = static_cast<QPushButton*>(sender());
+        pressed_button->setText(game_board_.at(pressed_button));
+        pressed_buttons_.push_back(pressed_button);
+        ++turned_cards_;
+    }
 }
 
 void MainWindow::handleCloseButtonClick()
@@ -80,7 +83,70 @@ void MainWindow::handleCloseButtonClick()
 
 void MainWindow::handleTurnBackButtonClick()
 {
-    close();
+    if (turned_cards_ == 2)
+    {
+        if (pressed_buttons_[0]->text() == pressed_buttons_[1]->text())
+        {
+            pressed_buttons_[0]->setVisible(false);
+            pressed_buttons_[1]->setVisible(false);
+            if (player_in_turn_ == "P1")
+            {
+                ++p1_points_;
+                std::string points_str = std::to_string(p1_points_);
+                QString points_qstr = QString::fromStdString("Player1: " + points_str);
+                ui->P1PointsLabel->setText(points_qstr);
+            }
+            else
+            {
+                ++p2_points_;
+                std::string points_str = std::to_string(p2_points_);
+                QString points_qstr = QString::fromStdString("Player2: " + points_str);
+                ui->P2PointsLabel->setText(points_qstr);
+            }
+        }
+        else
+        {
+            if (player_in_turn_ == "P1")
+            {
+                player_in_turn_ = "P2";
+                ui->CurrentTurnLabel->setText("Current turn: P2");
+            }
+            else
+            {
+                player_in_turn_ = "P1";
+                ui->CurrentTurnLabel->setText("Current turn: P1");
+            }
+            pressed_buttons_[0]->setText("");
+            pressed_buttons_[1]->setText("");
+
+        }
+        pressed_buttons_ = {};
+        turned_cards_ = 0;
+    }
+
+
+    if (p1_points_ + p2_points_ == CARD_AMOUNT/2)
+    {
+        if (p1_points_ > p2_points_)
+        {
+            ui->winnerLabel->setText("Player1 has won!");
+        }
+        else if (p2_points_ > p1_points_)
+        {
+            ui->winnerLabel->setText("Player2 has won!");
+        }
+        else
+        {
+            ui->winnerLabel->setText("It's a Draw");
+        }
+        ui->resetButton->setVisible(true);
+
+    }
+}
+
+void MainWindow::handeResetButtonClick()
+{
+
 }
 
 void MainWindow::init_game_board(const unsigned int& rows, const unsigned int& columns)
@@ -95,13 +161,32 @@ void MainWindow::init_game_board(const unsigned int& rows, const unsigned int& c
     {
         for (unsigned int c = 0; c < columns; ++c)
         {
+            char letter = letters_g_board_[r][c];
+            std::string letter_str;
+            letter_str += letter;
+            QString qstr = QString::fromStdString(letter_str);
             QPushButton* pushButton = new QPushButton(this);
+            game_board_.insert({pushButton, qstr});
             pushButton->setFixedSize(CARD_BUTTON_WIDTH, CARD_BUTTON_HEIGTH);
             grid->addWidget(pushButton,r,c);
             connect(pushButton, &QPushButton::clicked,
                     this, &MainWindow::handleCardClick);
         }
     }
+}
+
+void MainWindow::init_graphics_views()
+{
+    scene1_ = new QGraphicsScene(this);
+    ui->P1CardGraphicsView->setScene(scene1_);
+
+    scene2_ = new QGraphicsScene(this);
+    ui->P2CardsGraphicsView->setScene(scene2_);
+    /*QBrush redBrush(Qt::red);
+    QPen blackPen(Qt::black);
+    blackPen.setWidth(2);
+    rect_ = scene_->addRect(100,0,80,100,blackPen,redBrush);*/
+
 }
 
 
--- student/13/pairs_gui/mainwindow.hh
+++ student/13/pairs_gui/mainwindow.hh
@@ -4,6 +4,14 @@
 #include <QMainWindow>
 #include <vector>
 #include <string>
+#include <map>
+#include <QGridLayout>
+#include <QPushButton>
+#include <QGraphicsView>
+#include <QLabel>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsItem>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -21,18 +29,26 @@ private slots:
     void handleCardClick();
     void handleCloseButtonClick();
     void handleTurnBackButtonClick();
+    void handeResetButtonClick();
 
 private:
     Ui::MainWindow *ui;
 
-    std::vector<std::vector<char>> g_board_;
+    std::vector<std::vector<char>> letters_g_board_;
+    std::map<QPushButton*, QString> game_board_;
     std::string used_letters_;
+    QGraphicsScene* scene1_;
+    QGraphicsScene* scene2_;
 
     std::string player_in_turn_ = "P1";
     unsigned int turned_cards_ = 0;
+    std::vector<QPushButton*> pressed_buttons_;
 
-    const unsigned int MAX_CARD_AMOUNT = 16;
-    const std::string letters = "AABBCCDDEEFFGGHHIIJJ";
+    unsigned int p1_points_ = 0;
+    unsigned int p2_points_ = 0;
+
+    const unsigned int CARD_AMOUNT = 12;
+    const std::string letters = "AABBCCDDEEFFGGHHIIJJKKLLMMNNOOPP";
 
     const int CARD_BUTTON_WIDTH = 60;
     const int CARD_BUTTON_HEIGTH = 90;
@@ -40,6 +56,7 @@ private:
     void calculate_factors(unsigned int& smaller_factor, unsigned int& bigger_factor);
     void init_cards(const unsigned int& rows, const unsigned int& columns);
     void init_game_board(const unsigned int& rows, const unsigned int& columns);
+    void init_graphics_views();
 
 };
 #endif // MAINWINDOW_HH
